6eed6b8754c12706fbb0ad354cbf4325c2974953
liululu <443236745@qq.com>
2015-09-11 16:40:06 +0800

    two

--- src/demo/Test01.java
+++ src/demo/Test01.java
@@ -8,6 +8,8 @@ public class Test01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("hello world !!!!");
+		
 	}
 
 }
